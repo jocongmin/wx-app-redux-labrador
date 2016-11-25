fe1c16dcba7b0f53e62bd7c1f04e4a2064661d40
zhoucongmin <1641626308@qq.com>
2016-11-25 15:31:36 +0800

    redux 整理

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,18 +2,15 @@ import {
 	combineReducers
 } from 'redux';
 import configureStore from './createStore';
-import rootSaga from '../sagas/';
 
-import loginReducer from './login';
 import shopStateReducer from './shop';
 
 function createStore() {
 	const rootReducer = combineReducers({
-		login: loginReducer,
 		shopState: shopStateReducer,
 	});
 
-	return configureStore(rootReducer, rootSaga);
+	return configureStore(rootReducer);
 }
 
 export default createStore();
